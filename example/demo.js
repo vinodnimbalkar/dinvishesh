15ab16541bf55f4b4e72e205437f5e31dc2bff85
vinodnimbalkar <omvinod@yahoo.com>
2019-10-25 12:32:54 +0530

    example added

---
+++ example/demo.js
@@ -0,0 +1,9 @@
+const dinvishesh = require('dinvishesh');
+
+console.log(dinvishesh.dinVisheshToday());
+
+console.log(dinvishesh.dinVishesh("janm", "january", "11"));
+
+console.log(dinvishesh.dinVisheshAll());
+
+console.log(dinvishesh.dinVisheshByCategory("ghatana"));
