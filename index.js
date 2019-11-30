ef9d881ee3349e6cd684559a2e256df513e4ab00
vinodnimbalkar <omvinod@yahoo.com>
2019-11-30 18:48:37 +0530

    comment added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const dinVisheshData = require("./data/dinvishesh.json");
 
+/**
+ * A constant variable called 'monthNames'
+ * @constant {Array} - The name of months
+ */
 const monthNames = [
   "january",
   "february",
@@ -15,12 +19,34 @@ const monthNames = [
   "december"
 ];
 
+/**
+ * A constant variable called 'categoryList'
+ * @constant {Array}
+ */
 const categoryList = ["janm", "mrutyu", "ghatana"];
 
+/**
+ * A constant variable called 'date'
+ * @constant {Date}
+ */
 const date = new Date();
+
+/**
+ * A constant variable called 'currentday'
+ * @constant {number}
+ */
 const currentDay = date.getDate();
+
+/**
+ * A constant variable called 'monthNames'
+ * @constant {String}
+ */
 const currentMonth = monthNames[date.getMonth()];
 
+/**
+ * Retrives dinvishesh of current date
+ * @returns {Object}
+ */
 const dinVisheshToday = () => {
   dataToday = categoryList.map(
     cat => dinVisheshData.category[cat][currentMonth][currentDay]
@@ -33,6 +59,14 @@ const dinVisheshToday = () => {
   return responseData;
 };
 
+/**
+ * Retrives a dinvishesh by category, month, day
+ * @function
+ * @param {String} category
+ * @param {String} month
+ * @param {String} day
+ * @returns {Array}
+ */
 const dinVishesh = (category = "janm", month, day) => {
   if (
     category !== undefined &&
@@ -42,7 +76,6 @@ const dinVishesh = (category = "janm", month, day) => {
     day !== undefined &&
     day !== ""
   ) {
-    console.log(day);
     return dinVisheshData.category[category][month][day];
   } else if (
     category !== undefined &&
@@ -56,14 +89,26 @@ const dinVishesh = (category = "janm", month, day) => {
   }
 };
 
+/**
+ * Get all dinvishesh
+ * @returns {Object} - The dinvishesh contaning category
+ */
 const dinVisheshAll = () => {
   return dinVisheshData;
 };
 
+/**
+ * Retrives dinvishesh by category
+ * @param {String} category - The Category of dinvishesh e.g. janm, mrutyu, ghatana
+ * @returns {Object}
+ */
 const dinVisheshByCategory = category => {
   return dinVisheshData.category[category];
 };
 
+/**
+ * Module exports
+ */
 module.exports = {
   dinVisheshToday,
   dinVishesh,
